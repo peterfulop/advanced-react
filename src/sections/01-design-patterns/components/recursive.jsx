4ba53d1353e8d0321ff9b8244a77c5e0f853bb13
peterfulop <mrpeterfulop@gmail.com>
2023-11-20 11:02:43 +0100

    init section separation

--- src/sections/01-design-patterns/components/recursive.jsx
+++ src/sections/01-design-patterns/components/recursive.jsx
@@ -1,5 +1,17 @@
 const isObject = (data) => typeof data === 'object' && data !== null;
 
+export const myNestedObject = {
+    key1: 'value',
+    key2: {
+        innerKey1: 'innerValue1',
+        innerKey2: {
+            innerInnerKey1: 'innernInnerValue1',
+            innerInnerKey2: 'innernInnerValue2',
+        },
+    },
+    key3: 'value3',
+};
+
 export const RecursiceComponent = ({ data }) => {
     if (!isObject(data)) {
         return <li>{data}</li>;
